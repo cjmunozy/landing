32bd2346c8f3ca5fdea3e72746f0d37aa8f9ee58
cjmunozy <cjmunoz@espol.edu.ec>
2024-06-03 11:01:25 -0500

    Agregado archivo JS
    Creado archivo main.js y actualizado

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let loaded = ( eventLoaded ) => {
+
+    let myform = document.getElementById('formulario');
+    myform.addEventListener("submit", ( eventSubmit ) => { 
+
+        debugger;
+    
+      })
+}
+
+window.addEventListener("DOMContentLoaded", loaded);
+
