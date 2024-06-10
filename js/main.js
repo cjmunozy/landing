3687d0369addb9577b170374460eec816d058374
cjmunozy <cjmunoz@espol.edu.ec>
2024-06-10 10:57:50 -0500

    Agregada funcionalidad para enviar datos.
    Se añadió el envío de datos con medio de HTTPS POST

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 let loaded = ( eventLoaded ) => {  
-    let myform = document.getElementById('formulario');
+    let myform = document.getElementById("formulario");
     myform.addEventListener("submit", ( eventSubmit ) => {
-        
-        debugger;
-    
-      })
+        eventSubmit.preventDefault();
+        let nombreValue = nombre.value;
+        let emailValue = email.value;
+        if(nombreValue.length == 0 || emailValue == 0){
+            nombre.focus();
+            alert("Ingrese un texto válido.");
+            return;
+        }
+        let datos = {
+            nombre: nombreValue,
+            email: emailValue
+        }
+        fetch("https://coleccion-daw-default-rtdb.firebaseio.com/collection.json", {
+            method: "POST",
+            body: JSON.stringify(datos),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos);
+        })
+        .catch(error => console.error(error));
+        myform.reset();
+    })
 }
 
 window.addEventListener("DOMContentLoaded", loaded);
-
