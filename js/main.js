f595c19ff1f3d90a3130f89f633b35dbf8be402b
cjmunozy <cjmunoz@espol.edu.ec>
2024-06-19 06:35:15 -0500

    Integración con Firebase
    Se añadió la funcionalidad para enviar y recibir datos desde la base de datos. También se personalizó la página para que sea más acorde a la nueva temática

--- js/main.js
+++ js/main.js
@@ -2,16 +2,44 @@ let loaded = ( eventLoaded ) => {
     let myform = document.getElementById("formulario");
     myform.addEventListener("submit", ( eventSubmit ) => {
         eventSubmit.preventDefault();
+
+        let nombre = document.getElementById("nombre");
+        let email = document.getElementById("email");
+        let genero = document.getElementsByName("genero");
+        let inicial = document.getElementById("inicial");
+
         let nombreValue = nombre.value;
         let emailValue = email.value;
-        if(nombreValue.length == 0 || emailValue == 0){
+        let generoValue;
+        for (let i = 0; i < genero.length; i++) {
+            if (genero[i].checked) {
+                generoValue = genero[i].value;
+                break;
+            }
+        }
+        let inicialValue = inicial.value;
+
+        if(nombreValue.length == 0){
             nombre.focus();
             alert("Ingrese un texto válido.");
             return;
         }
+        if(emailValue.length == 0){
+            email.focus();
+            alert("Ingrese un texto válido.");
+            return;
+        }
+        if (inicialValue === "0") {
+            inicial.focus();
+            alert("Seleccione una opción.");
+            return;
+        }
+
         let datos = {
             nombre: nombreValue,
-            email: emailValue
+            email: emailValue,
+            genero: generoValue,
+            inicial: inicialValue
         }
         fetch("https://coleccion-daw-default-rtdb.firebaseio.com/collection.json", {
             method: "POST",
@@ -28,5 +56,52 @@ let loaded = ( eventLoaded ) => {
         myform.reset();
     })
 }
+
+async function cargarEstadisticas(){
+    const url = "https://coleccion-daw-default-rtdb.firebaseio.com/collection.json";
+    let respuesta = await fetch(url);
+    if (!respuesta.ok) {
+        console.error("Error:", respuesta.status);
+        return;
+    }
+    const datos = await respuesta.json();
+    return datos;
+}
+
+async function leerRegistros(){
+    let registros = await cargarEstadisticas();
+    let listaVotos = new Map();
+    for (let key in registros) {
+        if (registros.hasOwnProperty(key)) {
+            let registro = registros[key];
+            let pokemon = registro.inicial;
+            if (!listaVotos.has(pokemon)) {
+                listaVotos.set(pokemon, 1);
+            } else {
+                let votos = listaVotos.get(pokemon);
+                listaVotos.set(pokemon, votos + 1);
+            }
+        }
+    }
+    return listaVotos;
+}
+
+async function completarTabla(){
+    let votos = await leerRegistros();
+    let html = "";
+    votos.forEach( (voto, pokemon, map) => {
+        let plantilla = `
+            <tr>
+			    <td>${pokemon}</td>
+			    <td>${voto}</td>
+		    </tr>
+            `;
+        html += plantilla;
+    })
+    let tabla = document.getElementById("tablebody");
+    tabla.innerHTML = html;
+}
+
+completarTabla();
 
 window.addEventListener("DOMContentLoaded", loaded);
